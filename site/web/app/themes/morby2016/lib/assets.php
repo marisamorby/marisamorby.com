d47a2f4096be49d91238176e30f4fa09eac03047
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-05 22:48:00 +0900

    feat(assessment): add assessment assets when viewing the right template

--- site/web/app/themes/morby2016/lib/assets.php
+++ site/web/app/themes/morby2016/lib/assets.php
@@ -56,3 +56,18 @@ function asset_path($filename) {
     return $dist_path . $directory . $file;
   }
 }
+
+function include_assessment_maybe() {
+  if (is_page_template('template-assessment.php')) {
+    wp_dequeue_script('sage/js');
+    wp_enqueue_script('morby/assessment/js',
+      asset_path('scripts/assessment.js'),
+      [], null, true
+    );
+    wp_enqueue_style('morby/assessment/css',
+     asset_path('styles/assessment.css'),
+      ['sage/css'], null
+    );
+  }
+}
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\include_assessment_maybe', 101);
