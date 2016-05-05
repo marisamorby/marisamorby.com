52ec1dc68a6838e093e3cc4da5d56c9919a2812d
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-05 22:49:51 +0900

    feat(home): changed class name for credibility text

--- site/web/app/themes/morby2016/templates/credibility-text.php
+++ site/web/app/themes/morby2016/templates/credibility-text.php
@@ -1,5 +1,7 @@
-      <div class="credibility__section what-i-do">
+      <div class="credibility__section home-text">
         <h2 class="credibility__heading"><?php the_sub_field('heading'); ?></h2>
-        <?php the_sub_field('body_content'); ?>
+        <div class="home-text__text">
+          <?php the_sub_field('body_content'); ?>
 
+        </div>
       </div>
