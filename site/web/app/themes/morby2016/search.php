2810931dc4ecc43a6537d50795cb58aba0caf3fc
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-12 12:25:37 +0200

    feat(search): I think I could have just deleted this, but...

--- site/web/app/themes/morby2016/search.php
+++ site/web/app/themes/morby2016/search.php
@@ -1,14 +1,3 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'search'); ?>
-<?php endwhile; ?>
-
-<?php the_posts_navigation(); ?>
+require get_stylesheet_directory() . '/index.php';
