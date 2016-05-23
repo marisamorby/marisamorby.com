832c2941fc6281646b8c1586a77c548ab8959832
jlengstorf <jason.lengstorf@copterlabs.com>
2016-05-23 15:31:25 +0200

    feat(optimization): switched to Typekit async loading

--- site/web/app/themes/morby2016/templates/head.php
+++ site/web/app/themes/morby2016/templates/head.php
@@ -7,6 +7,14 @@
   <?php if (is_admin_bar_showing()): ?>
   <style>#wpadminbar{margin-top:0;}.floating-nav--fixed-top{top:46px}@media screen and (min-width: 783px){.floating-nav--fixed-top{top:32px}}</style>
   <?php endif; ?>
-  <script src="https://use.typekit.net/pxm2qtz.js"></script>
-  <script>try{Typekit.load({ async: true });}catch(e){}</script>
+  <script>
+    (function(d) {
+      var config = {
+        kitId: 'pxm2qtz',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
 </head>
