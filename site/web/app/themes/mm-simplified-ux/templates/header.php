3c8b5d180e6fb539c5fe9eb5a5c4c320326add66
Marisa <me@marisamorby.com>
2017-09-04 18:18:24 +0200

    header changes 09.04.2017

--- site/web/app/themes/mm-simplified-ux/templates/header.php
+++ site/web/app/themes/mm-simplified-ux/templates/header.php
@@ -2,11 +2,11 @@
   <nav class="top-nav__menu">
     <a href="/"
        class="top-nav__link top-nav__link--logo">Marisa Morby</a>
-    <a href="/resume/"
-       class="top-nav__link">Resume</a>
+    <a href="/talks-workshops/"
+       class="top-nav__link">talks & workshops</a>
     <a href="/about/"
-       class="top-nav__link">About</a>
+       class="top-nav__link">about</a>
     <a href="/contact/"
-       class="top-nav__link">Contact</a>
+       class="top-nav__link">contact</a>
   </nav>
 </header>
