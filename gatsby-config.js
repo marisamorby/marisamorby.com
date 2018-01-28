dc14e421cb52f53f3ceb12b5fc94c09855e33b8f
Jason Lengstorf <jason@lengstorf.com>
2018-01-28 10:31:34 -0600

    chore: initial repo setup

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: 'posts',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1380,
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+          },
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+  ],
+};
