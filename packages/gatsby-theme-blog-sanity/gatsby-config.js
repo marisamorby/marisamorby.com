7c3d688f2094abcbef4a537f257fe0ed67ed7e3b
Jason Lengstorf <jason@lengstorf.com>
2019-08-09 15:45:36 -0700

    fix: removed unused settings and magic

--- packages/gatsby-theme-blog-sanity/gatsby-config.js
+++ packages/gatsby-theme-blog-sanity/gatsby-config.js
@@ -1,21 +1,20 @@
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = ({ baseUrl = '', image = '', sanity = {} }) => ({
+module.exports = ({ baseUrl = '', sanity = {} }) => ({
   siteMetadata: {
     title: 'Gatsby Blog Powered By Sanity',
     description: 'This is a Gatsby blog that uses Sanity.io for writing posts.',
     baseUrl, // used to create absolute URLs for SEO
-    image, //
   },
   plugins: [
     'gatsby-plugin-theme-ui',
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId: sanity.projectId || process.env.GATSBY_SANITY_PROJECT_ID,
-        dataset: sanity.dataset || process.env.GATSBY_SANITY_DATASET,
-        token: sanity.token || process.env.SANITY_READ_TOKEN,
-        watchMode: sanity.watchMode || !isProd,
+        projectId: sanity.projectId,
+        dataset: sanity.dataset,
+        token: sanity.token,
+        watchMode: sanity.watchMode || (sanity.token && !isProd),
         overlayDrafts: sanity.overlayDrafts || !isProd,
       },
     },
