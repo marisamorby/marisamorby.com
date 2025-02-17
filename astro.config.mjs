fc20e91787d6a23276cd5096a1920ccc54a7a09d
Jason Lengstorf <jason@lengstorf.com>
2025-02-16 16:20:33 -0800

    wip: switch to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { imageService } from '@unpic/astro/service';
 
 // https://astro.build/config
-export default defineConfig({
-	image: {
-		service: imageService({
-			placeholder: 'blurhash',
-		}),
-	},
-});
+export default defineConfig({});
