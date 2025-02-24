472df810a709b33b6a53b59a93bce247797185eb
Jason Lengstorf <jason@lengstorf.com>
2025-02-23 21:44:10 -0800

    feat: add Unpic + Sanity client

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { imageService } from '@unpic/astro/service';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	image: {
+		service: imageService({
+			placeholder: 'blurhash',
+		}),
+	},
+});
